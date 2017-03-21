f821ba252cd844a4585b253db95ba5c78d703d8a
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-03-21 14:46:26 +0000

    Finish road counting logic

--- client/src/models/player.js
+++ client/src/models/player.js
@@ -22,28 +22,24 @@ class Player {
   addNextRoad(arrayOfRoads, lastNode) {
     const lastRoad = arrayOfRoads[arrayOfRoads.length - 1]
     arrayOfRoads = arrayOfRoads.slice()
-      // console.log("last node", lastNode)
       let surroundingRoadsOwned = []
       lastNode.surroundingRoads.forEach((surroundingRoad) => {
         if (surroundingRoad.colour === this.colour) {
           surroundingRoadsOwned.push(surroundingRoad)
         }
       })
-    // console.log("add next road")
     if (surroundingRoadsOwned.length === 1) {
       this.longestRoads.push(arrayOfRoads)
     } else {
-      console.log("surrounding roads owned", surroundingRoadsOwned)
       surroundingRoadsOwned.forEach((surrRoad) => {
-        console.log("surrRoad", surrRoad)
-        console.log("array of roads", arrayOfRoads)
         if (surrRoad !== lastRoad) {
-          arrayOfRoads.push(surrRoad)
+          ///////////// CLONE ARRAY FOR CASE OF ROAD SPLIT ////////////
+          let clonedArrayOfRoads = arrayOfRoads.slice()
+          clonedArrayOfRoads.push(surrRoad)
           const nodesSurroundingNewRoad = surrRoad.surroundingNodes
           nodesSurroundingNewRoad.forEach((node) => {
-            // console.log("node !== lastNode", node !== lastNode)
             if (node !== lastNode) {
-              this.addNextRoad(arrayOfRoads, node)
+              this.addNextRoad(clonedArrayOfRoads, node)
             }
           })
         }
@@ -52,27 +48,22 @@ class Player {
   }
 
   findLongestRoads() {
-    // console.log("find longest roads")
     this.roadsBuilt.forEach((road) => {
       road.surroundingNodes.forEach((node) => {
-        // console.log("node", node)
-
         let surroundingRoadsOwned = []
         node.surroundingRoads.forEach((surroundingRoad) => {
           if (surroundingRoad.colour === this.colour) {
             surroundingRoadsOwned.push(surroundingRoad)
           }
         })
-
+        ///////////// CHECK FOR THE STARTING ROAD //////////////////
         if (surroundingRoadsOwned.length === 1) {
           let arrayOfRoads = []
           arrayOfRoads.push(road)
           const nodes = road.surroundingNodes
           nodes.forEach((nodeInstance) => {
             if (nodeInstance !== node) {
-              // console.log("this", this)
               this.addNextRoad(arrayOfRoads, nodeInstance)
-
             }
           })
         }
