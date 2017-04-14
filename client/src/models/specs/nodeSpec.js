9924e7244fec585c4c526809dca31192f875b182
Nina <nkahr@Nina-Kahrs-MacBook-Pro.local>
2017-04-14 17:05:49 +0100

    add node tests

---
+++ client/src/models/specs/nodeSpec.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import Node from '../node.js';
+import 'babel-polyfill'
+
+describe("Node Tests", function(){
+
+  var node;
+
+  // beforeEach("Setup", function(){
+  //   player = new Player({name: "John", colour: "blue"})
+  // })
+
+  // it("should have a name", function(){
+  //   assert.equal("John", player.name)
+  // })
+
+})
