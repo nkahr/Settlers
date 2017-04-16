aac9bff471d1e042e66b61a5d0ded92ea8c9d475
Nina <ntkahr@gmail.com>
2017-04-16 10:00:57 +0200

    bank should generate resource card test

--- client/src/models/specs/bankSpec.js
+++ client/src/models/specs/bankSpec.js
@@ -1,6 +1,7 @@
 
 import assert from 'assert';
 import Bank from '../bank.js';
+import ResourceCard from '../resourceCard.js';
 import 'babel-polyfill'
 
 describe("Bank Tests", function(){
@@ -13,6 +14,11 @@ describe("Bank Tests", function(){
 
   it("number of development cards should not be zero", function(){
     assert.notEqual(0, bank.developmentCards.length)
+  })
+
+  it("should be able to generate resource card", function(){
+    const sheep = new ResourceCard("sheep")
+    assert.deepEqual(sheep, bank.generateResourceCard("sheep"))
   })
 
 })
