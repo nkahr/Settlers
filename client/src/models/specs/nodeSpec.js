98c4be8a903778d40442a29a77a4efb94288f85e
Nina <ntkahr@gmail.com>
2017-04-16 21:08:38 +0200

    node should have a port test

--- client/src/models/specs/nodeSpec.js
+++ client/src/models/specs/nodeSpec.js
@@ -1,17 +1,20 @@
 import assert from 'assert';
 import Node from '../node.js';
+import Port from '../port.js';
 import 'babel-polyfill'
 
 describe("Node Tests", function(){
 
   var node;
+  var port;
 
-  // beforeEach("Setup", function(){
-  //   player = new Player({name: "John", colour: "blue"})
-  // })
+  beforeEach("Setup", function(){
+    port = new Port({coordinates: [392, 598], type: "three_to_one", angle: 30})
+    node = new Node({coordinates: [392, 598], port: port})
+  })
 
-  // it("should have a name", function(){
-  //   assert.equal("John", player.name)
-  // })
+  it("should have a port", function(){
+    assert.equal(port, node.port)
+  })
 
 })
