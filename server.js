742e56ac8ada7d009e63690ea78b481e1f5d7f53
Nina <ntkahr@gmail.com>
2017-04-24 10:03:15 +0200

    use socket.io

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-var express = require('express');
+var express = require('express')
 var app = express();
-var path = require('path')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// http.listen(app.get('port')); 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
 app.use(express.static('client/build'));
 
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
