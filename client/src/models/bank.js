295332323e3d0a2e9eac7d917d4965e9d91759ef
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-04-27 12:41:37 +0100

    remove dev card from bank when bought by player

--- client/src/models/bank.js
+++ client/src/models/bank.js
@@ -16,6 +16,7 @@ class Bank {
     const length = this.developmentCards.length
     const randomIndex = Math.floor(Math.random() * length)  
     const devCard = this.developmentCards[randomIndex ]
+    this.developmentCards.splice(randomIndex, 1)
     return devCard
   }
 
@@ -27,6 +28,14 @@ class Bank {
     for (let i = 0; i < 2; i++) {
       const newDevCard = new DevelopmentCard({type: "roadBuilding"}) 
       this.developmentCards.push(newDevCard)
+    }
+    for (let i = 0; i < 15; i++) {
+
+
+
+
+
+
     }
     // for (let i = 0; i < 2; i++) {
     //   const newDevCard = new DevelopmentCard({type: "monopoly"}) 
